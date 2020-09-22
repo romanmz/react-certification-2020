32bde0c89cb82869062a3aa1925c80a276f77595
Roman Martinez <roman.martinez@wizeline.com>
2020-09-21 21:45:32 -0500

    Deleted everything inside src except <HomePage />

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,32 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import AuthProvider from '../../providers/Auth';
 import HomePage from '../../pages/Home';
-import LoginPage from '../../pages/Login';
-import NotFound from '../../pages/NotFound';
-import SecretPage from '../../pages/Secret';
-import Private from '../Private';
 
 function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route exact path="/login">
-            <LoginPage />
-          </Route>
-          <Private exact path="/secret">
-            <SecretPage />
-          </Private>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </AuthProvider>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </BrowserRouter>
   );
 }
