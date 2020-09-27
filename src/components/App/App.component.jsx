59835cf14fd1d0070213fe3202a343b185783f49
Roman Martinez <roman.martinez@wizeline.com>
2020-09-27 14:55:15 -0500

    Create and setup UserContext

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
+
+// Contexts
+import UserContext from '../../state/UserContext';
 
 // Pages
 import HomePage from '../../pages/Home';
 import VideoPage from '../../pages/Video';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
+    <UserContext.Provider value={{ user, setUser }}>
     <BrowserRouter>
         <header>
+          {user ? (
+            <>
+              <h3>Welcome {user.name}</h3>
+              <img src={user.avatarUrl} alt="user avatar" />
+            </>
+          ) : (
+            ''
+          )}
           <nav>
             <ul>
               <li>
@@ -24,6 +38,7 @@ function App() {
         <Route path="/video/:id" component={VideoPage} />
       </Switch>
     </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
