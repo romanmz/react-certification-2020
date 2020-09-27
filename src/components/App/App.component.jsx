75d4f7e6bb0b03319e4aac9b0dfe6de1ed74ddf5
Roman Martinez <roman.martinez@wizeline.com>
2020-09-27 16:00:17 -0500

    Create and link <FavoritesPage /> page

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
+import PrivateRoute from '../PrivateRoute';
 
 // Contexts
 import UserContext from '../../state/UserContext';
@@ -8,6 +9,7 @@ import UserContext from '../../state/UserContext';
 import HomePage from '../../pages/Home';
 import VideoPage from '../../pages/Video';
 import LoginPage from '../../pages/Login';
+import FavoritesPage from '../../pages/Favorites';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -31,6 +33,11 @@ function App() {
                   Home
                 </NavLink>
               </li>
+              {user && (
+                <li>
+                  <NavLink to="/favorites">Favorites</NavLink>
+                </li>
+              )}
               <li>
                 <NavLink to="/login">Login</NavLink>
               </li>
@@ -41,6 +48,7 @@ function App() {
           <Route exact path="/" component={HomePage} />
           <Route path="/video/:id" component={VideoPage} />
           <Route path="/login" component={LoginPage} />
+          <PrivateRoute path="/favorites" component={FavoritesPage} />
         </Switch>
       </BrowserRouter>
     </UserContext.Provider>
---
+++ src/pages/Favorites/Favorites.page.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import UserContext from '../../state/UserContext';
+import VideoItem from '../../components/VideoItem';
+
+const FavoritesPage = () => {
+  const { user } = useContext(UserContext);
+  const favs = user.favorites ?? [];
+
+  // Render
+  return (
+    <>
+      <h1>Favorites</h1>
+      {favs.length === 0 ? (
+        <p>No favorite videos yet</p>
+      ) : (
+        favs.map((video) => <VideoItem {...video} />)
+      )}
+    </>
+  );
+};
+
+export default FavoritesPage;
