7eae3b14e4c9b93c1bc1eaa47065d2a18e7d3b0f
Roman Martinez <roman.martinez@wizeline.com>
2020-09-28 18:15:01 -0500

    Install styled-components, and create <GlobalStyles />

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import PrivateRoute from '../PrivateRoute';
 
 // Contexts
@@ -16,6 +17,17 @@ import LoginPage from '../../pages/Login';
 import LogoutPage from '../../pages/Logout';
 import FavoritesPage from '../../pages/Favorites';
 
+// Styles
+import GlobalStyles from './GlobalStyles';
+
+const lightTheme = {
+  color: {
+    text: '#333',
+    background: 'white',
+  },
+};
+
+// APP
 function App() {
   const [user, setUser] = useState(null);
   const [keyword, setKeyword] = useState('wizeline');
@@ -23,17 +35,20 @@ function App() {
   return (
     <UserContext.Provider value={{ user, setUser }}>
       <SearchContext.Provider value={{ keyword, setKeyword }}>
-        <BrowserRouter>
+        <ThemeProvider theme={lightTheme}>
+          <BrowserRouter>
+            <GlobalStyles />
             <SiteHeader />
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/search/:keyword" component={HomePage} />
-            <Route path="/video/:id" component={VideoPage} />
-            <Route path="/login" component={LoginPage} />
-            <Route path="/logout" component={LogoutPage} />
-            <PrivateRoute path="/favorites" component={FavoritesPage} />
-          </Switch>
-        </BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/search/:keyword" component={HomePage} />
+              <Route path="/video/:id" component={VideoPage} />
+              <Route path="/login" component={LoginPage} />
+              <Route path="/logout" component={LogoutPage} />
+              <PrivateRoute path="/favorites" component={FavoritesPage} />
+            </Switch>
+          </BrowserRouter>
+        </ThemeProvider>
       </SearchContext.Provider>
     </UserContext.Provider>
   );
---
+++ src/components/App/GlobalStyles.js
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'polished';
+
+// Global styles
+const GlobalStyles = createGlobalStyle`
+  ${normalize()}
+
+  // Document
+  // ------------------------------
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    font-size: 1.6rem;
+    line-height: 1.8;
+    font-family: Helvetica, Arial, sans-serif;
+    color: ${({ theme }) => theme.color.text}
+    background-color: ${({ theme }) => theme.color.background};
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  ::selection {
+    color: white;
+    background-color: ${({ theme }) => theme.color.text};
+    text-shadow: none;
+  }
+
+  // Default spacing for block elements
+  // ------------------------------
+  p, h1, h2, h3, h4, h5, h6,
+  ul, ol, dl, table, pre, blockquote,
+  fieldset, legend, figure, details {
+    margin: 0 0 2rem;
+  }
+
+  // Embedded media
+  // ------------------------------
+  img, iframe,
+  video, audio, canvas, svg,
+  embed, object {
+    box-sizing: border-box;
+    max-width: 100%;
+  }
+  img {
+    height: auto;
+  }
+`;
+
+export default GlobalStyles;
