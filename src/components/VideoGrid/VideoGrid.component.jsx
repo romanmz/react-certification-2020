4089fab3c769f1b2f7814b7bf5dfd3af8313a934
Roman Martinez <roman.martinez@wizeline.com>
2020-09-28 19:00:21 -0500

    Add basic styles for video grid

--- src/components/VideoGrid/VideoGrid.component.jsx
+++ src/components/VideoGrid/VideoGrid.component.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 import VideoItem from '../VideoItem';
+
+const VideoGridContainer = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+
+  li {
+    flex: 0 0 50%;
+  }
+`;
 
 const VideoGrid = ({ videos }) => {
   return (
-    <ul>
+    <VideoGridContainer>
       {videos.map((video) => (
         <li>
           <VideoItem {...video} key={video.id} />
         </li>
       ))}
-    </ul>
+    </VideoGridContainer>
   );
 };
 
--- src/components/VideoItem/VideoItem.component.jsx
+++ src/components/VideoItem/VideoItem.component.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const VideoItemContainer = styled.div`
+  a {
+    display: flex;
+    margin: 2rem;
+    background: white;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+
+    text-decoration: none;
+    color: ${(props) => props.theme.color.text};
+    transition: all 0.5s;
+  }
+  a:hover {
+    background: #eee;
+    transform: translateY(-1rem);
+  }
+  img {
+    flex: 0 0 50%;
+  }
+  div {
+    flex: auto;
+    padding: 2rem;
+  }
+`;
 
 const VideoItem = ({ id, title, description, thumbnail }) => {
   return (
-    <div>
+    <VideoItemContainer>
       <Link to={`/video/${id}`}>
         <img src={thumbnail} height="360" width="480" alt={title} />
-        <h3>{title}</h3>
-        <p>{description}</p>
+        <div>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
       </Link>
-    </div>
+    </VideoItemContainer>
   );
 };
 
